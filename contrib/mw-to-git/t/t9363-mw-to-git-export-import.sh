0d75d9d4648063db78948223b93e1bbe72757d86
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2020-09-16 12:29:18 +0200

    remote-mediawiki tests: annotate failing tests
    These tests consistently fail for me, and were failing before any of
    the changes in this series. As noted in [1] there are some known
    intermittent test failures. Let's mark these as failing so we can have
    an otherwise passing test suite.
    
    1. https://github.com/Git-Mediawiki/Git-Mediawiki/issues/56
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- contrib/mw-to-git/t/t9363-mw-to-git-export-import.sh
+++ contrib/mw-to-git/t/t9363-mw-to-git-export-import.sh
@@ -32,7 +32,7 @@ test_expect_success 'setup config' '
 	test_might_fail git config --global --unset remote.origin.mediaImport
 '
 
-test_expect_success 'git push can upload media (File:) files' '
+test_expect_failure 'git push can upload media (File:) files' '
 	wiki_reset &&
 	git clone mediawiki::'"$WIKI_URL"' mw_dir &&
 	(
@@ -48,7 +48,7 @@ test_expect_success 'git push can upload media (File:) files' '
 	)
 '
 
-test_expect_success 'git clone works on previously created wiki with media files' '
+test_expect_failure 'git clone works on previously created wiki with media files' '
 	test_when_finished "rm -rf mw_dir mw_dir_clone" &&
 	git clone -c remote.origin.mediaimport=true \
 		mediawiki::'"$WIKI_URL"' mw_dir_clone &&
